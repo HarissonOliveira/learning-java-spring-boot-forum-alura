c03c7fbcda5845c5e83b6dbd4c709c8a07156d17
Harisson Oliveira <harisson.batista.oliveira@everis.com>
2021-09-22 11:31:11 -0300

    Added Pagination for TopicosController

--- src/main/java/br/com/harisson/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/harisson/forum/repository/TopicoRepository.java
@@ -1,6 +1,9 @@
 package br.com.harisson.forum.repository;
 
 import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +12,7 @@ import br.com.harisson.forum.models.Topico;
 
 public interface TopicoRepository extends JpaRepository<Topico, Long> {
 
-	public List<Topico> findByCursoNomeContains(String nomeCurso);
+	public Page<Topico> findByCursoNomeContains(String nomeCurso, Pageable paginacao);
 	
 	// EXEMPLO GERANDO A QUERY
 	@Query("SELECT t FROM Topico t WHERE t.curso.nome = :nomeCurso")
